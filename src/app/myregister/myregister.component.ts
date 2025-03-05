4ce1606b7068b653d2bed060f13829e6d243db53
GallaHemanth <119731994+GallaHemanth@users.noreply.github.com>
2025-03-05 13:12:51 +0530

    Add files via upload

--- src/app/myregister/myregister.component.ts
+++ src/app/myregister/myregister.component.ts
@@ -1,22 +1,53 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MyloginComponent } from '../mylogin/mylogin.component';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-myregister',
-  imports: [],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './myregister.component.html',
   styleUrl: './myregister.component.css'
 })
-export class MyregisterComponent {
-  constructor(private router: Router) { }
-  login() {
-    // add user verification logic
-    this.router.navigate(['mylogin']);
-  }
-  register(){
-    this.router.navigate(['myregister']);
-  }
+// export class MyregisterComponent {
+  export class MyregisterComponent { 
+    reactiveForm : FormGroup;
+  
+    constructor(private router: Router, private registerform: FormBuilder) {
+      this.router = router; 
+      this.reactiveForm = this.registerform.group({
+        fullName: ['', [Validators.required, Validators.pattern('^[a-zA-Z\\s]+$')]],
+        gender: ['',  Validators.required],
+        email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')]],
+        mobilenumber: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+        dateofbirth: ['', [Validators.required]],
+        coursetype: ['', [Validators.required]],
+        terms: [false, Validators.requiredTrue]
+      });
+    }
+
+    get fullName() { return this.reactiveForm.get('fullName'); }
+    get gender() { return this.reactiveForm.get('gender'); }
+    get email() { return this.reactiveForm.get('email'); }
+    get mobilenumber() { return this.reactiveForm.get('mobilenumber'); }
+    get dateofbirth() { return this.reactiveForm.get('dateofbirth'); }
+    get coursetype() { return this.reactiveForm.get('coursetype'); }
+    get terms() { return this.reactiveForm.get('terms'); }
+  
+    onSubmit() {
+      console.log(this.reactiveForm.value);
+    }
+
+    login() {
+      this.router.navigate(['mylogin']);
+    }
+  
+    register() {
+      this.router.navigate(['myregister']);
+    }
+  
   // email: string = '';
   // otp: string = '';
   // showOtpInput: boolean = false;
@@ -61,4 +92,5 @@ export class MyregisterComponent {
   //     }
   //   );
   // }
+
 }
