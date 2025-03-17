d272b02db41d66fcf0c116c14db2f224dfe1334f
GallaHemanth <119731994+GallaHemanth@users.noreply.github.com>
2025-03-17 11:51:09 +0530

    Update app-routing.module.ts

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,11 +5,17 @@ import { MyloginComponent } from '../mylogin/mylogin.component';
 import { MyregisterComponent } from '../myregister/myregister.component';
 import { Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Form01Component } from '../form01/form01.component';
+import { PerformComponent } from '../perform/perform.component';
+import { QualformComponent } from '../qualform/qualform.component';
 
  const routes: Routes = [
   { path: '', redirectTo: '/mylogin', pathMatch: 'full' }, 
   { path: 'mylogin', component: MyloginComponent },
   { path: 'myregister', component: MyregisterComponent },
+  { path: 'form01', component: Form01Component },
+    {path: 'perform', component: PerformComponent},
+    {path: 'qualform', component: QualformComponent}
 ];
 
 @NgModule({
